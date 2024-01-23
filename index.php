3ac1828c796c3e96f89864c0ce765845cc8fb9ba
Josiah <s10244345@connect.np.edu.sg>
2024-01-23 14:54:50 +0800

    lion city lush

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>ECAD ASG INDEX</title>
+<title>LION CITY LUSH</title>
 <link href="css/bootstrap.min.css" rel="stylesheet">

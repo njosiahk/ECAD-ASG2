38c533cac18f28476c48d7eafee2b85aa2c98a47
Kee-Wei-HengNp <yw3ih3ng2005@gmail.com>
2024-01-23 15:21:42 +0800

    checklogin changes

---
+++ checklogin.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+ $email  = $_POST["email"];
+ $pwd = $_post["password"];
+
+ if(($email == "ecader@np.edu.sg") && ($pwd == "ecader")){
+    $_SESSION["ShopperName"] = "Ecader";
+    $_SESSION["ShopperID"] = 1;
+    echo "You are a valid user"
+ }
+ else{
+    echo "You are an invalid user!"
+ }
+ ?>

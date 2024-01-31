b8c3707e0ca60b33529b1348301a316c29615849
Giovanni Raynard Salim <S10242618@connect.np.edu.sg>
2024-01-31 19:58:38 +0800

    CSS STYLE

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <?php 
 //Display guest welcome message, Login and Registration links
 //when shopper has yet to login,
-$content1 = "Welcome Guest<br />";
+$content1 = "Welcome ";
 // href='register.php'
 $content2 = "<li class='nav-item'>
              <a class='nav-link' >Sign Up</a></li>
@@ -17,7 +17,7 @@ if(isset($_SESSION["ShopperName"])) {
     else {
         $cartid = "not created";
     }
-    $content1 = "Welcome <b>$_SESSION[ShopperName] $_SESSION[ShopperID] </b>";//ID:$_SESSION[ShopperID] cartid:$cartid
+    $content1 = "Welcome $_SESSION[ShopperName] ID: $_SESSION[ShopperID] ";//ID:$_SESSION[ShopperID] cartid:$cartid
     $content2 = "<li class='nav-item'>
                  <a class='nav-link' href='changePassword.php'>Change Password</a></li>
                  <li class='nav-item'>
@@ -25,10 +25,11 @@ if(isset($_SESSION["ShopperName"])) {
     
 
     //Display number of item in cart
-    if (isset($_SESSION["NumCartItem"])) {
-        $content1 .= " $_SESSION[NumCartItem] item(s) in shopping cart";
-    }
+    // if (isset($_SESSION["NumCartItem"])) {
+    //     $content1 .= " $_SESSION[NumCartItem] item(s) in shopping cart";
+    // }
 }
+
 ?>
 
 
@@ -48,36 +49,28 @@ if(isset($_SESSION["ShopperName"])) {
                 </div>
             </div>
             <div class="container px-0">
+              <h5 class="navbar-nav mx-auto"><?php echo $content1 ?></h5>
+            </div>
+            <div class="container px-0">
                 <nav class="navbar navbar-light bg-white navbar-expand-xl">
-                    <a href="index.html" class="navbar-brand"><h1 class="text-primary display-6">Fruitables</h1></a>
+
+                    <a href="index.php" class="navbar-brand"><h1 class="text-primary display-6">Lion City Lush</h1></a>
                     <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                         <span class="fa fa-bars text-primary"></span>
                     </button>
+                    
                     <div class="collapse navbar-collapse bg-white" id="navbarCollapse">
                         <div class="navbar-nav mx-auto">
-                            <a href="index.html" class="nav-item nav-link active">Home</a>
-                            <a href="shop.html" class="nav-item nav-link">Shop</a>
-                            <a href="shop-detail.html" class="nav-item nav-link">Shop Detail</a>
-                            <div class="nav-item dropdown">
-                                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                                <div class="dropdown-menu m-0 bg-secondary rounded-0">
-                                    <a href="cart.html" class="dropdown-item">Cart</a>
-                                    <a href="chackout.html" class="dropdown-item">Chackout</a>
-                                    <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                                    <a href="404.html" class="dropdown-item">404 Page</a>
-                                </div>
-                            </div>
-                            <a href="contact.html" class="nav-item nav-link">Contact</a>
+                            <a href="index.php" class="nav-item nav-link active">Home</a>
+                            <a href="category.php" class="nav-item nav-link">Product Types</a>
                         </div>
                         <div class="d-flex m-3 me-0">
-                            <button class="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fas fa-search text-primary"></i></button>
-                            <a href="#" class="position-relative me-4 my-auto">
+                            <a href="search.php"><button class="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4" ><i class="fas fa-search text-primary"></i></button></a>
+                            <a href="shoppingCart.php" class="position-relative me-4 my-auto">
                                 <i class="fa fa-shopping-bag fa-2x"></i>
-                                <span class="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1" style="top: -5px; left: 15px; height: 20px; min-width: 20px;">3</span>
+                                <span class="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1" style="top: -5px; left: 15px; height: 20px; min-width: 20px;"><?php echo $_SESSION["NumCartItem"] ?></span>
                             </a>
-                            <a href="#" class="my-auto">
-                                <i class="fas fa-user fa-2x"></i>
-                            </a>
+                            
                         </div>
                     </div>
                 </nav>
--- shoppingCart.php
+++ shoppingCart.php
@@ -5,7 +5,7 @@ include("header.php");
 
 if (! isset($_SESSION["ShopperID"])) { // Check if user logged in 
     // redirect to login page if the session variable shopperid is not set
-    header ("Location: login.php");
+    echo '<script>window.location.href = "login.php";</script>';
     exit;
 }
 
